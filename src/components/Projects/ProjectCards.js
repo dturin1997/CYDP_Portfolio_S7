7fb1e39112399f595ed2c43d784b49097964d13d
Dario Turin <dturin1997@gmail.com>
2023-05-27 23:02:41 -0500

    Se abilitó el enlace a repo de github

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -13,7 +13,7 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button disabled variant="primary" href={props.ghLink} target="_blank">
+        <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
